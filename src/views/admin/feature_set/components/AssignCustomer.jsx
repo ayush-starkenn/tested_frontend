f994404e849a94eebd2cb14a6417f3a57eb66470
sapnaRL <sapna@starkenn.com>
2023-08-28 18:41:31 +0530

    Feature Set added

--- src/views/admin/feature_set/components/AssignCustomer.jsx
+++ src/views/admin/feature_set/components/AssignCustomer.jsx
@@ -1,65 +1,106 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { Dropdown } from "primereact/dropdown";
 import { Button } from "primereact/button";
 import { useRef } from "react";
 import { Toast } from "primereact/toast";
+import Cookies from "js-cookie";
+import { AppContext } from "context/AppContext";
 
 const AssignCustomer = ({ parameters, onSuccess }) => {
-  const [data, setData] = useState([]);
-  const [selectedCustomer, setSelectedCustomer] = useState(null);
+  const [currentUsers, setCurrentUsers] = useState([]);
+  const [newUser, setNewUser] = useState();
+  const [listCustomers, setListCustomers] = useState([]);
+  const [featuresetUsers, setFeaturesetUsers] = useState([]);
   const toastErr = useRef(null);
   const [validationError, setValidationError] = useState(false);
+  const { updateFunc } = useContext(AppContext);
 
-  //fetching customers to assign
+  const token = Cookies.get("token");
+
   useEffect(() => {
     axios
       .get(
-        `${process.env.REACT_APP_API_URL}/featureset/featureset-not-assign-customerlist/${parameters.propValue}`
+        `http://localhost:8080/api/featuresets/get-list-assign-users/${parameters.propValue.featureset_uuid}`,
+        {
+          headers: { authorization: `bearer ${token}` },
+        }
       )
       .then((res) => {
-        setData(res.data);
+        setCurrentUsers(res.data.results);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, [parameters.propValue]);
-  const customerOptions = data.map((customer) => ({
-    label: `${customer.first_name} ${customer.last_name}`,
-    value: customer.userId,
-  }));
+  }, [token, parameters.propValue.featureset_uuid]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/customers/get-all-customer", {
+        headers: { authorization: `bearer ${token}` },
+      })
+      .then((res) => {
+        setListCustomers(res.data.customers);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [token]);
+
+  useEffect(() => {
+    if (currentUsers) {
+      let mapCurrentUsers = currentUsers.map((el) => el.user_uuid);
+
+      let usersList = listCustomers.filter((el) =>
+        mapCurrentUsers.includes(el.user_uuid)
+      );
+      setFeaturesetUsers(usersList);
+    }
+  }, [listCustomers, currentUsers]);
 
   //update and assign FS to customer
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!selectedCustomer) {
+    if (!newUser) {
       setValidationError(true);
       return;
     }
-    const requestData = {
-      selectCustomer: [selectedCustomer],
-    };
-
     axios
       .put(
-        `${process.env.REACT_APP_API_URL}/featureset/featureset-assign-customer/${parameters.propValue}`,
-        requestData
+        `http://localhost:8080/api/featuresets/assign-user/${parameters.propValue.featureset_uuid}`,
+        newUser,
+        {
+          headers: { authorization: `bearer ${token}` },
+        }
       )
       .then((res) => {
         console.log(res);
+        updateFunc();
+
         if (onSuccess) {
           onSuccess();
         }
       })
       .catch((err) => {
-        console.error(err);
-        toastErr.current.show({
-          severity: "danger",
-          summary: "Error",
-          detail: err.response.data.message || "An error occurred",
-          life: 3000,
-        });
+        console.log(err);
       });
+  };
+  const handleChange = (e) => {
+    setNewUser(e.value);
+    setNewUser({ [e.target.name]: e.value });
+  };
+  const userOptions = () => {
+    if (!featuresetUsers) {
+      return [];
+    }
+    return featuresetUsers
+      .filter((el) => el.user_type === 2)
+      .map((el) => ({
+        label: el.first_name + " " + el.last_name,
+        value: {
+          user_uuid: el.user_uuid,
+        },
+      }));
   };
 
   return (
@@ -69,9 +110,13 @@ const AssignCustomer = ({ parameters, onSuccess }) => {
         <form onSubmit={handleSubmit}>
           <span className="p-float-label mt-8">
             <Dropdown
-              value={selectedCustomer}
-              options={customerOptions}
-              onChange={(e) => setSelectedCustomer(e.value)}
+              name="featureset_user"
+              value={newUser && newUser.featureset_user}
+              options={userOptions()}
+              // onChange={(e) => {
+              //   setNewUser({ [e.target.name]: e.value }); // Use e.value and e.target.name
+              // }}
+              onChange={handleChange}
               className={`rounded-lg border border-gray-300 bg-gray-50 py-0 shadow-sm dark:bg-gray-900 dark:placeholder-gray-50 ${
                 validationError ? "p-invalid" : ""
               }`}
